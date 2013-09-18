783687df79be4c98d613ee994b9cf3b92cd69a93
smjason <scott_jason@harvard.edu>
2013-09-17 20:39:34 -0400

    Made some changes to the Bio body

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <h1>Project One</h1>
 <p>&nbsp;</p>
 <h2> Scott Jason Bio</h2>
-<p>I have been in the information technology field for around 14 years. I have created wesites along the way and have taken various classes that will count towards my computer science degree at the Harvard Extension School. I am happy to be in a field that I enjoy as it is challenging and also ever changing. </p>
+<p>I have been in the information technology field for around fourteen years. I have created wesites along the way and have taken various classes that will count towards my computer science degree at the Harvard Extension School. I am happy to be in a field that I enjoy as it is challenging and also ever changing. I am also blessed to working at Harvard University amongst so many brilliant people.</p>
 <p>This is a simple proof of concept site created with Dreamweaver CS5.</p>
 <p>
     <a href="http://jigsaw.w3.org/css-validator/check/referer">
