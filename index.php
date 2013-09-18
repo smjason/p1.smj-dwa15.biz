52f37a4f628b0ce72ab7ab1c320c328789a43055
smjason <scott_jason@harvard.edu>
2013-09-17 21:29:16 -0400

    Fixed spelling errors and added text to the body as well as added the HTML validation link

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <h1>Project One</h1>
 <p>&nbsp;</p>
 <h2> Scott Jason Bio</h2>
-<p>I have been in the information technology field for around fourteen years. I have created wesites along the way and have taken various classes that will count towards my computer science degree at the Harvard Extension School. I am happy to be in a field that I enjoy as it is challenging and also ever changing. I am also blessed to working at Harvard University amongst so many brilliant people.</p>
+<p>I have been in the information technology field for around fourteen years. I have created websites along the way and have taken various classes that will count towards my computer science degree at the Harvard Extension School. I am happy to be in a field that I enjoy as it is both challenging and ever-changing. I am also blessed to be working at Harvard University amongst so many brilliant people. I am looking forward to what this class has to offer.</p>
 <p>This is a simple proof of concept site created with Dreamweaver CS5.</p>
 <p>
     <a href="http://jigsaw.w3.org/css-validator/check/referer">
@@ -19,6 +19,8 @@
             src="http://jigsaw.w3.org/css-validator/images/vcss"
             alt="Valid CSS!" />
     </a>
+    
+<p><a href="http://validator.w3.org/check?uri=http%3A%2F%2Fwww.p1.smj-dwa15.biz%2F">Click</a> for HTML validation</p>
 </p>
 </body>
 </html>
