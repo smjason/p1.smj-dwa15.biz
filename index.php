3c1798db4db277200c8adc8d7a38b0db263909d9
smjason <scott_jason@harvard.edu>
2013-09-17 20:34:49 -0400

    Adding Index file for project One

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+<title>Project One</title>
+</head>
+
+<body>
+
+<h1>Project One</h1>
+<p>&nbsp;</p>
+<h2> Scott Jason Bio</h2>
+<p>I have been in the information technology field for around 14 years. I have created wesites along the way and have taken various classes that will count towards my computer science degree at the Harvard Extension School. I am happy to be in a field that I enjoy as it is challenging and also ever changing. </p>
+<p>This is a simple proof of concept site created with Dreamweaver CS5.</p>
+<p>
+    <a href="http://jigsaw.w3.org/css-validator/check/referer">
+        <img style="border:0;width:88px;height:31px"
+            src="http://jigsaw.w3.org/css-validator/images/vcss"
+            alt="Valid CSS!" />
+    </a>
+</p>
+</body>
+</html>
