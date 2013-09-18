9573cd8a5a69de359c500a41827e5758cfc50b2e
smjason <scott_jason@harvard.edu>
2013-09-17 21:41:02 -0400

    Fixed a validation error in index page

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <link rel="stylesheet" type="text/css" href="styles.css" />
+<link rel="stylesheet" type="text/css" href="styles.css" />
 <title>Project One</title>
 </head>
 
@@ -14,13 +14,17 @@
 <p>I have been in the information technology field for around fourteen years. I have created websites along the way and have taken various classes that will count towards my computer science degree at the Harvard Extension School. I am happy to be in a field that I enjoy as it is both challenging and ever-changing. I am also blessed to be working at Harvard University amongst so many brilliant people. I am looking forward to what this class has to offer.</p>
 <p>This is a simple proof of concept site created with Dreamweaver CS5.</p>
 <p>
-    <a href="http://jigsaw.w3.org/css-validator/check/referer">
+    
+    
+   <a href="http://jigsaw.w3.org/css-validator/check/referer">
         <img style="border:0;width:88px;height:31px"
             src="http://jigsaw.w3.org/css-validator/images/vcss"
             alt="Valid CSS!" />
-    </a>
-    
-<p><a href="http://validator.w3.org/check?uri=http%3A%2F%2Fwww.p1.smj-dwa15.biz%2F">Click</a> for HTML validation</p>
+</a></p>
+<p>
+  
+  <a href="http://validator.w3.org/check?uri=http%3A%2F%2Fwww.p1.smj-dwa15.biz%2F">Click</a> for HTML validation
+  
 </p>
 </body>
 </html>
